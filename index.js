f1b0f53ce26d4668461a9b0e354e52ca90a650da
L C <lindner.christoph01@gmail.com>
2023-06-05 07:10:55 +0200

    1
    1

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+// import inquirer from "inquirer";
+// import fs from "fs";
+import os from "os";
+// import chalk from "chalk";
+
+
+//A////////////
+// Require os module
+const os = require('os');
+ 
+// Printing os.platform() value
+console.log(process.platform);
+//E/////////////
